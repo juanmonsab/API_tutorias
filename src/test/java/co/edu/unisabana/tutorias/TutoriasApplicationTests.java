6684c2094062c43f146d306c3b326af27eaf53a0
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2023-05-29 06:45:26 -0500

    Primer commit

---
+++ src/test/java/co/edu/unisabana/tutorias/TutoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unisabana.tutorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
