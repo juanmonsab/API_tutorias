6134cadba10aebad466f39c613df0c1f00503c93
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2023-05-29 09:14:39 -0500

    Crea los constructores

--- src/main/java/co/edu/unisabana/tutorias/Estudiante.java
+++ src/main/java/co/edu/unisabana/tutorias/Estudiante.java
@@ -1,4 +1,28 @@
 package co.edu.unisabana.tutorias;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Estudiante {
-}
+    private int codigo;
+    private String nombre;
+    private String facultad;
+    private String programa;
+    private int semestre;
+    private List<String> habilidades;
+    private List<Disponibilidad> disponibilidad;
+
+    // Constructor vacío
+    public Estudiante() {
+    }
+
+    // Constructor con parámetros
+    public Estudiante(int codigo, String nombre, String facultad, String programa, int semestre, List<String> habilidades, List<Disponibilidad> disponibilidad) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.facultad = facultad;
+        this.programa = programa;
+        this.semestre = semestre;
+        this.habilidades = habilidades;
+        this.disponibilidad = disponibilidad;
+    }
