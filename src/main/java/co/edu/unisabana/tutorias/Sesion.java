fcbab22ec4cc25e11b7690d7e503510cabee1f21
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2023-05-29 09:22:24 -0500

    cCrea los constructores

--- src/main/java/co/edu/unisabana/tutorias/Sesion.java
+++ src/main/java/co/edu/unisabana/tutorias/Sesion.java
@@ -1,4 +1,19 @@
 package co.edu.unisabana.tutorias;
 
 public class Sesion {
-}
+    private int codigo;
+    private Estudiante tutor;
+    private String dia;
+    private String horarioInicio;
+    private String horarioFin;
+
+    public Sesion() {
+    }
+
+    public Sesion(int codigo, Estudiante tutor, String dia, String horarioInicio, String horarioFin) {
+        this.codigo = codigo;
+        this.tutor = tutor;
+        this.dia = dia;
+        this.horarioInicio = horarioInicio;
+        this.horarioFin = horarioFin;
+    }
