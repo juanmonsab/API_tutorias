6969766e30b8faad64e034e7ed1a942e4ac7fd66
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2023-05-29 09:25:29 -0500

    Crea los constructores

--- src/main/java/co/edu/unisabana/tutorias/Disponibilidad.java
+++ src/main/java/co/edu/unisabana/tutorias/Disponibilidad.java
@@ -1,4 +1,13 @@
 package co.edu.unisabana.tutorias;
 
 public class Disponibilidad {
-}
+    private String dia;
+    private String horario;
+
+    public Disponibilidad() {
+    }
+
+    public Disponibilidad(String dia, String horario) {
+        this.dia = dia;
+        this.horario = horario;
+    }
