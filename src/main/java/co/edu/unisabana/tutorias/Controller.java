f3ef8ab28c8d23875f65fe4f2ace606dd146b81d
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2023-05-29 09:03:40 -0500

    Importa lo necesario ye implementa funcion para crear estudiante

--- src/main/java/co/edu/unisabana/tutorias/Controller.java
+++ src/main/java/co/edu/unisabana/tutorias/Controller.java
@@ -1,4 +1,26 @@
 package co.edu.unisabana.tutorias;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+@RestController
+@RequestMapping("/estudiantes")
 public class Controller {
-}
+
+    private static final List<Estudiante> estudiantes = new ArrayList<>();
+    private static final List<Sesion> sesiones = new ArrayList<>();
+
+    @PostMapping("/crear")
+    public Respuesta agregarEstudiante(@RequestBody Estudiante estudiante) {
+        Random rnd = new Random();
+        int codigo = 100000 + rnd.nextInt(900000); // Generar código aleatorio de 6 dígitos
+        estudiante.setCodigo(codigo);
+        estudiantes.add(estudiante);
+        return new Respuesta("Se ha creado el estudiante correctamente");
+    }
